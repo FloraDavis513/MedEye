edba063c0ce94d15292da4e50ed0bc882aeeacee
railbotan@mail.ru <railbotan@mail.ru>
2022-10-10 22:06:08 +0500

    Add random move target and blink

--- Views/Following.axaml.cs
+++ Views/Following.axaml.cs
@@ -14,13 +14,19 @@ public partial class Following : Window
     // Random generator for target pos.
     private static readonly Random Rnd = new Random();
 
-    private static readonly DispatcherTimer Tmr = new DispatcherTimer();
+    private static readonly DispatcherTimer MoveTargetTimer = new DispatcherTimer();
+    private static readonly DispatcherTimer DirectRotationTimer = new DispatcherTimer();
+    private static readonly DispatcherTimer TargetBlinkTimer = new DispatcherTimer();
+    private static readonly DispatcherTimer StalkerBlinkTimer = new DispatcherTimer();
 
     // Counter for current color.
     private int _currentColor = 0;
 
-    private int _dx = 3;
-    private int _dy = 3;
+    //For moving target.
+    private int _direct = 0;
+    private double _lenD = 2;
+    private double _dx = 0;
+    private double _dy = 0;
 
     // Alias for colors amblyopia array.
     private readonly IBrush[] _colors = ColorConst.AMBLYOPIA_COLORS;
@@ -30,13 +36,53 @@ public partial class Following : Window
         #if DEBUG
                 this.AttachDevTools();
         #endif
-        Tmr.Tick += MoveTarget;
-        Tmr.Interval = new TimeSpan(10000);
+        
         Canvas.SetTop(Target, Rnd.Next(0, Convert.ToInt32(this.ClientSize.Height - Target.Height)));
         Canvas.SetLeft(Target, Rnd.Next(0, Convert.ToInt32(this.ClientSize.Width - Target.Width)));
-        Tmr.Start();
+        
+        Canvas.SetTop(Angle, 10);
+        Canvas.SetLeft(Angle, 10);
+        
+        Canvas.SetTop(DX, 30);
+        Canvas.SetLeft(DX, 10);
+        
+        Canvas.SetTop(DY, 50);
+        Canvas.SetLeft(DY, 10);
+        
+        _direct = Rnd.Next(90);
+        _dx = _lenD * Math.Cos(_direct);
+        _dy = _lenD * Math.Sin(_direct);
+        StartTimer();
     }
 
+    private void StartTimer()
+    {
+        MoveTargetTimer.Tick += MoveTarget;
+        MoveTargetTimer.Interval = new TimeSpan(20000);
+        
+        DirectRotationTimer.Tick += DirectRotation;
+        DirectRotationTimer.Interval = new TimeSpan(0, 0, 0, 1);
+        
+        TargetBlinkTimer.Tick += TargetBlink;
+        TargetBlinkTimer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+        
+        StalkerBlinkTimer.Tick += StalkerBlink;
+        StalkerBlinkTimer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+        
+        MoveTargetTimer.Start();
+        DirectRotationTimer.Start();
+        TargetBlinkTimer.Start();
+        StalkerBlinkTimer.Start();
+    }
+    
+    private void StopTimer()
+    {
+        MoveTargetTimer.Stop();
+        DirectRotationTimer.Stop();
+        TargetBlinkTimer.Stop();
+        StalkerBlinkTimer.Stop();
+    }
+    
     private void MoveTarget(object? sender, EventArgs e)
     {
         var targetX = Canvas.GetLeft(Target);
@@ -45,22 +91,52 @@ public partial class Following : Window
         if (targetX + _dx <= 0 || targetX + _dx >= this.ClientSize.Width - Target.Width)
         {
             _dx = -_dx;
+            _direct += 90;
         }
  
         if (targetY + _dy <= 0 || targetY + _dy  >= ClientSize.Height - Target.Height)
         {
             _dy = -_dy;
+            _direct += 90;
         }
         
         Canvas.SetLeft(Target, targetX + _dx);
         Canvas.SetTop(Target, targetY + _dy);
+        Angle.Text = _direct.ToString();
+        DX.Text = _dx.ToString();
+        DY.Text = _dy.ToString();
+    }
+
+    private void DirectRotation(object? sender, EventArgs e)
+    {
+        _direct = _direct - 15 + (Rnd.NextDouble() > 0.5 ? 0 : 30);
+        _direct %= 360;
+        _dx = _lenD * Math.Cos(_direct * Math.PI / 180);
+        _dy = _lenD * Math.Sin(_direct * Math.PI / 180);
+        Angle.Text = _direct.ToString();
+        DX.Text = _dx.ToString();
+        DY.Text = _dy.ToString();
+    }
+    
+    
+
+    private void TargetBlink(object? sender, EventArgs e)
+    {
+        Target.Background = Equals(Target.Background, _colors[_currentColor]) 
+            ? ColorConst.AMBLYOPIA_MOVE_COLOR 
+            : _colors[_currentColor];
+    }
+    private void StalkerBlink(object? sender, EventArgs e) {
+        Stalker.BorderBrush = Equals(Stalker.BorderBrush, _colors[_currentColor]) 
+            ? ColorConst.AMBLYOPIA_MOVE_COLOR 
+            : _colors[_currentColor];
     }
 
     protected override void OnKeyDown(KeyEventArgs e)
     {
         if (e.Key == Key.Escape)
         {
-            Tmr.Stop();
+            StopTimer();
             this.Close();
         }
         base.OnKeyDown(e);
@@ -74,19 +150,6 @@ public partial class Following : Window
     
     protected override void OnPointerMoved(PointerEventArgs e)
     {
-        if (Target.BorderBrush == _colors[_currentColor])
-            Target.BorderBrush = ColorConst.AMBLYOPIA_MOVE_COLOR;
-        else
-            Target.BorderBrush = _colors[_currentColor];
-        
-        
-        // Honestly, crutch. Need a better solution. 
-        if (Stalker.Background == _colors[_currentColor])
-            Stalker.Background = ColorConst.AMBLYOPIA_MOVE_COLOR;
-        else
-            Stalker.Background = _colors[_currentColor];
-        
-
         var currentPosition = e.GetPosition(Parent);
         var offsetX = currentPosition.X;
         var offsetY = currentPosition.Y;
