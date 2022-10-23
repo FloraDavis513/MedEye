9242290ba96727e663081e804c1a33de8e35206a
FloraDavis513 <alexfeofilov513@gmail.com>
2022-10-23 17:20:02 +0500

    - fix menu adapt - add modal window

--- Views/MainMenu.axaml.cs
+++ Views/MainMenu.axaml.cs
@@ -1,42 +1,38 @@
 using Avalonia;
-using Avalonia.Collections;
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.LogicalTree;
 using Avalonia.Media.Imaging;
-using Avalonia.Svg;
+using Avalonia.Threading;
 using System;
 
 namespace MedEye.Views
 {
     public partial class MainMenu : Window
     {
+        private static readonly DispatcherTimer close_timer = new DispatcherTimer();
+
         public MainMenu()
         {
             InitializeComponent();
 
             var button = this.FindControl<Button>("Exit");
+            button.Click += ExitClick;
 
-            button.Click += Button_Click;
+            var button2 = this.FindControl<Button>("Cabinet");
+            button2.Click += CabinetClick;
+
+            var button3 = this.FindControl<Button>("Registry");
+            button3.Click += RegistryClick;
+
+            close_timer.Tick += CloseAfterRoute;
+            close_timer.Interval = new TimeSpan(10000000);
         }
 
         protected override void OnOpened(EventArgs e)
         {
-            var button_width = 2 * this.ClientSize.Width / 9;
-
-            var reg = this.Get<Button>("Registry");
-            var cab = this.Get<Button>("Cabinet");
-            var settings = this.Get<Button>("Settings");
-            var exit = this.Get<Button>("Exit");
-
-            reg.Width = button_width;
-            cab.Width = button_width;
-            settings.Width = button_width;
-            exit.Width = button_width;
+            AdaptToScreen();
 
             var logo = this.Get<Image>("Logo");
-
             logo.Source = new Bitmap("C:\\Users\\Alex\\source\\repos\\MedEye\\Assets\\logo.png");
 
             base.OnOpened(e);
@@ -56,18 +52,56 @@ namespace MedEye.Views
 
             if (change.Property.Name == "WindowState" || change.Property.Name == "Width")
             {
-                var button_width = 2 * this.ClientSize.Width / 9;
-                reg.Width = button_width;
-                cab.Width = button_width;
-                settings.Width = button_width;
-                exit.Width = button_width;
+                AdaptToScreen();
             }
             base.OnPropertyChanged(change);
         }
 
-        private void Button_Click(object? sender, RoutedEventArgs e)
+        private void ExitClick(object? sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void CabinetClick(object? sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            close_timer.Start();
+        }
+
+        private void RegistryClick(object? sender, RoutedEventArgs e)
+        {
+            new ModalWindow().Show();
+        }
+
+        private void CloseAfterRoute(object? sender, EventArgs e)
+        {
+            this.Close();
+            close_timer.Stop();
+        }
+
+        private void AdaptToScreen()
+        {
+            var button_width = 2 * this.ClientSize.Width / 9;
+
+            var reg = this.Get<Button>("Registry");
+            var cab = this.Get<Button>("Cabinet");
+            var settings = this.Get<Button>("Settings");
+            var exit = this.Get<Button>("Exit");
+
+            reg.Width = button_width;
+            cab.Width = button_width;
+            settings.Width = button_width;
+            exit.Width = button_width;
+
+            reg.FontSize = 32 * (this.ClientSize.Width / 1920);
+            cab.FontSize = 32 * (this.ClientSize.Width / 1920);
+            settings.FontSize = 32 * (this.ClientSize.Width / 1920);
+            exit.FontSize = 32 * (this.ClientSize.Width / 1920);
+
+            var h1 = this.Get<TextBlock>("MainHeader");
+            var h2 = this.Get<TextBlock>("SubHeader");
+            h1.FontSize = 96 * (this.ClientSize.Width / 1920);
+            h2.FontSize = 48 * (this.ClientSize.Width / 1920);
         }
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Interactivity;
 using Avalonia.Controls;
 
 
@@ -9,9 +10,49 @@ namespace MedEye.Views
         public MainWindow()
         {
             InitializeComponent();
+            var tyr = this.FindControl<Button>("Tyr");
+            tyr.Click += TyrClick;
+
+            var following = this.FindControl<Button>("Following");
+            following.Click += FollowingClick;
+
+            var merger = this.FindControl<Button>("Merger");
+            merger.Click += MergerClick;
+
+            var combination = this.FindControl<Button>("Combination");
+            combination.Click += CombinationClick;
+
+            var exit = this.FindControl<Button>("Exit");
+            exit.Click += ExitClick;
+
             #if DEBUG
                 this.AttachDevTools();
             #endif
+        }
+
+        private void TyrClick(object? sender, RoutedEventArgs e)
+        {
+            new Tyr().Show();
+        }
+
+        private void FollowingClick(object? sender, RoutedEventArgs e)
+        {
+            new Following().Show();
+        }
+
+        private void MergerClick(object? sender, RoutedEventArgs e)
+        {
+            new Merger().Show();
+        }
+
+        private void CombinationClick(object? sender, RoutedEventArgs e)
+        {
+            new Combination().Show();
+        }
+
+        private void ExitClick(object? sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
