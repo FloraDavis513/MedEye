29fcb3db0cd9a437a6121216c109b25a6c0920e7
railbotan@mail.ru <railbotan@mail.ru>
2022-10-24 22:04:17 +0500

    Add settings control

--- Views/MainMenu.axaml.cs
+++ Views/MainMenu.axaml.cs
@@ -16,7 +16,6 @@ namespace MedEye.Views
             InitializeComponent();
 
             var button = this.FindControl<Button>("Exit");
-            Cabinet.Click += (sender, args) => new SetupMenu().Show();
             button.Click += ExitClick;
 
             var button2 = this.FindControl<Button>("Cabinet");
@@ -26,7 +25,7 @@ namespace MedEye.Views
             button3.Click += RegistryClick;
 
             close_timer.Tick += CloseAfterRoute;
-            close_timer.Interval = new TimeSpan(10000000);
+            close_timer.Interval = new TimeSpan(1000000);
         }
 
         protected override void OnOpened(EventArgs e)
@@ -66,7 +65,7 @@ namespace MedEye.Views
 
         private void CabinetClick(object? sender, RoutedEventArgs e)
         {
-            new MainWindow().Show();
+            new SetupMenu().Show();
             close_timer.Start();
         }
 
@@ -83,17 +82,17 @@ namespace MedEye.Views
 
         private void AdaptToScreen()
         {
-            var button_width = 2 * this.ClientSize.Width / 9;
+            var buttonWidth = 2 * this.ClientSize.Width / 9;
 
             var reg = this.Get<Button>("Registry");
             var cab = this.Get<Button>("Cabinet");
             var settings = this.Get<Button>("Settings");
             var exit = this.Get<Button>("Exit");
 
-            reg.Width = button_width;
-            cab.Width = button_width;
-            settings.Width = button_width;
-            exit.Width = button_width;
+            reg.Width = buttonWidth;
+            cab.Width = buttonWidth;
+            settings.Width = buttonWidth;
+            exit.Width = buttonWidth;
 
             reg.FontSize = 32 * (this.ClientSize.Width / 1920);
             cab.FontSize = 32 * (this.ClientSize.Width / 1920);
--- Views/SetupMenu.axaml.cs
+++ Views/SetupMenu.axaml.cs
@@ -1,16 +1,97 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace MedEye.Views;
 
 public partial class SetupMenu : Window
 {
+    private static readonly DispatcherTimer closeTimer = new DispatcherTimer();
+    
     public SetupMenu()
     {
         InitializeComponent();
 #if DEBUG
         this.AttachDevTools();
 #endif
+
+        MainMenu.Click += MainMenuClick;
+        StartGame.Click += StartGameClick;
+        
+        closeTimer.Tick += CloseAfterRoute;
+        closeTimer.Interval = new TimeSpan(1000000);
+    }
+
+    protected override void OnOpened(EventArgs e)
+    {
+        AdaptToScreen();
+
+        base.OnOpened(e);
+    }
+
+    private void MainMenuClick(object? sender, RoutedEventArgs e)
+    {
+        new MainMenu().Show();
+        closeTimer.Start();
+    }
+    
+    private void CloseAfterRoute(object? sender, EventArgs e)
+    {
+        Close();
+        closeTimer.Stop();
+    }
+    
+    private void StartGameClick(object? sender, RoutedEventArgs e)
+    {
+        var game = Game1.SelectedIndex;
+        switch (game)
+        {
+            case 0:
+                new Tyr().Show();
+                break;
+            case 1:
+                new Following().Show();
+                break;
+            case 2:
+                new Combination().Show();
+                break;
+            case 3:
+                new Merger().Show();
+                break;
+        }
+    }
+
+    private void AdaptToScreen()
+    {
+        var buttonWidth = 2 * this.ClientSize.Width / 9;
+
+        Game1.Width = buttonWidth;
+        MainMenu.Width = buttonWidth;
+        DeleteGame.Width = buttonWidth;
+        StartGame.Width = buttonWidth;
+        FrequencyFlickerBox.Width = buttonWidth;
+        TypeFlickerBox.Width = buttonWidth;
+
+        Game1.FontSize = 32 * (this.ClientSize.Width / 1920);
+        MainMenu.FontSize =  32 * (this.ClientSize.Width / 1920);
+        DeleteGame.FontSize =  32 * (this.ClientSize.Width / 1920);
+        StartGame.FontSize =  32 * (this.ClientSize.Width / 1920);
+        Distance.FontSize =  32 * (this.ClientSize.Width / 1920);
+        LeftFilterColor.FontSize =  32 * (this.ClientSize.Width / 1920);
+        FrequencyFlickerText.FontSize =  32 * (this.ClientSize.Width / 1920);
+        FrequencyFlickerBox.FontSize =  32 * (this.ClientSize.Width / 1920);
+        TypeFlickerText.FontSize =  32 * (this.ClientSize.Width / 1920);
+        TypeFlickerBox.FontSize =  32 * (this.ClientSize.Width / 1920);
+        BrightRedColor.FontSize =  32 * (this.ClientSize.Width / 1920);
+        BrightBlueColor.FontSize =  32 * (this.ClientSize.Width / 1920);
+        Level.FontSize =  32 * (this.ClientSize.Width / 1920);
+        Timer.FontSize =  32 * (this.ClientSize.Width / 1920);
+
+        Header1.FontSize = 48 * (this.ClientSize.Width / 1920);
+        Header2.FontSize = 48 * (this.ClientSize.Width / 1920);
     }
 }
