e9a6cfc8881543d11f4a34b83ec04e09a85f4589
Zarathustra616 <65862257+Zarathustra616@users.noreply.github.com>
2022-10-26 18:14:47 +0500

    Add files via upload
    Timer

--- example.py
+++ example.py
@@ -2,12 +2,16 @@
 Demonstration of the GazeTracking library.
 Check the README.md for complete documentation.
 """
-
+import time
 import cv2
 from gaze_tracking import GazeTracking
 
+start_time = time.time()
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
+
+end_time = 0
+total_time = 0
 
 while True:
     # We get a new frame from the webcam
@@ -19,14 +23,15 @@ while True:
     frame = gaze.annotated_frame()
     text = ""
 
-    if gaze.is_blinking():
-        text = "Blinking"
-    elif gaze.is_right():
+
+    if gaze.is_right():
         text = "Looking right"
     elif gaze.is_left():
         text = "Looking left"
     elif gaze.is_center():
         text = "Looking center"
+    elif gaze.is_blinking():
+        text = "Blinking"
 
     cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
@@ -37,8 +42,15 @@ while True:
 
     cv2.imshow("Demo", frame)
 
+    if (gaze.is_right() or gaze.is_left() or gaze.is_center()):
+        end_time = time.time()-start_time
+        start_time = time.time()
+    total_time += end_time
+    end_time = 0
+
+
     if cv2.waitKey(1) == 27:
         break
-   
+print(total_time)
 webcam.release()
 cv2.destroyAllWindows()
