b8b5d2ff23f1a66a7528a04b25c3709eafaf5948
FloraDavis513 <alexfeofilov513@gmail.com>
2022-12-06 20:52:36 +0500

    - add gamer selector - add adaptive to modal windows

--- Views/ConfirmAction.axaml.cs
+++ Views/ConfirmAction.axaml.cs
@@ -6,6 +6,9 @@ namespace MedEye.Views
 {
     public partial class ConfirmAction : Window
     {
+        private double windowWidth;
+        private double windowHeight;
+
         public ConfirmAction()
         {
             InitializeComponent();
@@ -24,22 +27,42 @@ namespace MedEye.Views
             Yes.Click += ExitClick;
         }
 
+        public ConfirmAction(double main_window_width, double main_window_height, string message, EventHandler<RoutedEventArgs> act)
+        {
+            InitializeComponent();
+
+            windowWidth = 1055 * (main_window_width / 1920);
+            windowHeight = 600 * (main_window_height / 1080);
+
+            ConfirmText.Text = message;
+
+            No.Click += ExitClick;
+            Yes.Click += act;
+            Yes.Click += ExitClick;
+        }
+
         private void ExitClick(object? sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        protected override void OnOpened(EventArgs e)
+        {
+            AdaptToScreen();
+            base.OnOpened(e);
+        }
+
         private void AdaptToScreen()
         {
-            var buttonWidth = 2 * this.ClientSize.Width / 9;
+            this.Width = windowWidth;
+            this.Height = windowHeight;
+            var buttonWidth = this.ClientSize.Width / 3;
 
-            Yes.Width = buttonWidth;
             No.Width = buttonWidth;
-
-            Yes.FontSize = 32 * (this.ClientSize.Width / 1920);
-            No.FontSize = 32 * (this.ClientSize.Width / 1920);
-
-            ConfirmText.FontSize = 48 * (this.ClientSize.Width / 1920);
+            Yes.Width = buttonWidth;
+            Yes.FontSize = 36 * (this.ClientSize.Width / windowWidth);
+            No.FontSize = 36 * (this.ClientSize.Width / windowWidth);
+            ConfirmText.FontSize = 48 * (this.ClientSize.Width / windowWidth);
         }
     }
 }
--- Views/ModalWindow.axaml.cs
+++ Views/ModalWindow.axaml.cs
@@ -6,6 +6,12 @@ namespace MedEye.Views
 {
     public partial class ModalWindow : Window
     {
+        private double windowWidth;
+        private double windowHeight;
+        private double mainWindowWidth;
+        private double mainWindowHeight;
+        private EventHandler<RoutedEventArgs> act;
+
         public ModalWindow()
         {
             InitializeComponent();
@@ -17,14 +23,51 @@ namespace MedEye.Views
             button2.Click += WithoutClick;
         }
 
+        public ModalWindow(double main_window_width, double main_window_height, EventHandler<RoutedEventArgs> act)
+        {
+            InitializeComponent();
+
+            mainWindowWidth = main_window_width;
+            mainWindowHeight = main_window_height;
+            windowWidth = 1055 * (main_window_width / 1920);
+            windowHeight = 600 * (main_window_height / 1080);
+            this.act = act;
+
+            With.Click += WithClick;
+
+            Without.Click += WithoutClick;
+            Without.Click += act;
+        }
+
         private void WithClick(object? sender, RoutedEventArgs e)
         {
+            new GamerSelector(mainWindowWidth, mainWindowHeight, act).Show();
             this.Close();
         }
 
         private void WithoutClick(object? sender, RoutedEventArgs e)
         {
+            new SetupMenu(-1).Show();
             this.Close();
+        }
+
+        protected override void OnOpened(EventArgs e)
+        {
+            AdaptToScreen();
+            base.OnOpened(e);
+        }
+
+        private void AdaptToScreen()
+        {
+            this.Width = windowWidth;
+            this.Height = windowHeight;
+            var buttonWidth = this.ClientSize.Width / 3;
+
+            With.Width = buttonWidth;
+            Without.Width = buttonWidth;
+            With.FontSize = 48 * (this.ClientSize.Width / windowWidth);
+            Without.FontSize = 48 * (this.ClientSize.Width / windowWidth);
+            Header.FontSize = 96 * (this.ClientSize.Width / windowWidth);
         }
     }
 }
